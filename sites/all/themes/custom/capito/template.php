589d94eba9457fadd62a7dcc68e366abb2018994
marwenblel <marwen.blel@gmail.com>
2024-05-18 22:59:38 +0200

    Updating template.php and new-ad-page.css

--- sites/all/themes/custom/capito/template.php
+++ sites/all/themes/custom/capito/template.php
@@ -18,6 +18,8 @@ function capito_process_html(&$variables) {
  * DO2: add external awesome css library when theme_get_settings('font_awesome') = 1.
  */
 function capito_preprocess_html(&$variables) {
+    global $user;
+
     //DO1
     if(theme_get_setting('capito_font_awesome') == 1) {
         drupal_add_css('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', array('type' => 'external'));
@@ -34,9 +36,17 @@ function capito_preprocess_html(&$variables) {
         drupal_add_js('https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js', array('type' => 'external'));
         drupal_add_js('https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js', array('type' => 'external'));
     }
+    // DO4: add class "user role" to body tag
+    if (in_array('particular', $user->roles)) {
+        // Add the CSS class to the body.
+        $variables['classes_array'][] = 'particular';
+    }
+    elseif (in_array('professional', $user->roles)) {
+        // Add the CSS class to the body.
+        $variables['classes_array'][] = 'professional';
+    }
 
 }
-
 /**
  * Implements hook_preprocess_page().
  * @param $vars
